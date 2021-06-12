67cb9a051816bbecd41a135875905b163c5a7a68
Ankit Srivastava <ankitsrivas14@gmail.com>
2021-06-12 16:06:37 +0530

    added bootstrap table

--- src/components/features/Dmrc.vue
+++ src/components/features/Dmrc.vue
@@ -1,14 +1,11 @@
 <template>
-  <SCLayout eventType="User Identification">
+  <SCLayout :eventDetails="getModeData">
     <template slot="header-cta">
       <button class="primary-btn">Update Biometrics</button>
       <button class="secondary-btn">Update Biometrics</button>
     </template>
     <template slot="body">
-      <img
-        class="layout-main-image"
-        :src="require('@/assets/static/features/aadhar.jpg')"
-      />
+      <b-table striped hover :items="items"></b-table>
     </template>
   </SCLayout>
 </template>
@@ -17,6 +14,25 @@ import SCLayout from "@/components/shared/SCLayout";
 export default {
   components: {
     SCLayout,
+  },
+  computed: {
+    getModeData() {
+      return {
+        eventName: "Transport",
+        modeName: "DMRC Transactions",
+        icon: "bus",
+      };
+    },
+  },
+  data() {
+    return {
+      items: [
+        { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
+        { age: 21, first_name: "Larsen", last_name: "Shaw" },
+        { age: 89, first_name: "Geneva", last_name: "Wilson" },
+        { age: 38, first_name: "Jami", last_name: "Carney" },
+      ],
+    };
   },
 };
 </script>
