1b084056e0173f6979708c5ad607b6cfa5db7721
Ankit Srivastava <ankitsrivas14@gmail.com>
2021-06-20 16:14:23 +0530

    current bill and contact provider completed

--- src/components/features/Electricity/ElectricityDashboard.vue
+++ src/components/features/Electricity/ElectricityDashboard.vue
@@ -4,15 +4,75 @@
       <Bar class="bar-chart" />
       <LineChart class="line-chart" />
     </div>
+    <div class="electricity-btns">
+      <ElectricityOptions
+        @past-bills="handleButtonClick('pastBills')"
+        @current-bills="handleButtonClick('currentBill')"
+        @live-usage="handleButtonClick('liveUsage')"
+        @contact-provider="handleButtonClick('contactProvider')"
+        @recharge="handleButtonClick('recharge')"
+      />
+    </div>
+    <div class="electricity-options__container">
+      <div v-if="options.currentBill" class="options__current-bill">
+        <span
+          >Latest generated bill has been sent to your registered email id.<br />
+          <b>{{ email }}</b>
+        </span>
+      </div>
+      <div v-if="options.contactProvider" class="options__contact">
+        <span>
+          <b>Electricity Provider:</b> BSES Yamuna<br /><br />
+          <b>Helpline:</b> 011-39999808<br /><br />
+          <b>Emergency:</b> 011-41999808<br /><br />
+          <b>Address:</b> For BSES Yamuna Power Ltd.<br />
+          Key Consumer Cell, Sub-station<br />
+          No.15, 9th Avenue, I.P. Extension, <br />
+          Patparganj, Delhi-110092, India<br />
+          011-39997005
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import Bar from "@/components/features/Electricity/Charts/Bar";
 import LineChart from "@/components/features/Electricity/Charts/LineChart";
+import ElectricityOptions from "@/components/features/Electricity/ElectricityOptions";
+
+import { mapGetters } from "vuex";
+
 export default {
+  data() {
+    return {
+      options: null,
+    };
+  },
   components: {
     Bar,
     LineChart,
+    ElectricityOptions,
+  },
+  computed: {
+    ...mapGetters(["email"]),
+  },
+  methods: {
+    handleButtonClick(btnType) {
+      this.resetOptions();
+      this.options[btnType] = true;
+    },
+    resetOptions() {
+      this.options = {
+        pastBills: false,
+        currentBill: false,
+        liveUsage: false,
+        contactProvider: false,
+        recharge: false,
+      };
+    },
+  },
+  mounted() {
+    this.resetOptions();
   },
 };
 </script>
@@ -20,8 +80,23 @@ export default {
 .electricity-container {
   .charts-container {
     display: flex;
+    margin-bottom: 20px;
     .bar-chart {
-      // width: 80%;
+      width: 90%;
+    }
+  }
+  .electricity-options__container {
+    .options__current-bill {
+      display: grid;
+      place-items: center;
+      height: 100px;
+      text-align: center;
+    }
+    .options__contact {
+      text-align: left;
+      display: flex;
+      justify-content: center;
+      margin-top: 50px;
     }
   }
 }
---
+++ src/components/features/Electricity/ElectricityOptions.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="bank__btn-list">
+    <button @click="$emit('past-bills')">Past Bills</button>
+    <button @click="$emit('current-bills')">Current Bill</button>
+    <button @click="$emit('live-usage')">Live Usage</button>
+    <button @click="$emit('contact-provider')">Contact Provider</button>
+    <button class="major-button" @click="$emit('recharge')">
+      Recharge
+    </button>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+.bank__btn-list {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+  button {
+    border: none;
+    padding: 10px 18px;
+    margin-right: 12px;
+    border-radius: 4px;
+    &:hover {
+      background-color: #e2e2e2;
+    }
+  }
+  .major-button {
+    background-color: #007700;
+    color: #f6f7fb;
+    &:hover {
+      background-color: #028802;
+    }
+  }
+}
+</style>
